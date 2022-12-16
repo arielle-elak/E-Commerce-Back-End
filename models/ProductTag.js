c36e7923ecaa8f04c683d9510130d4d36de7dd5c
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-15 20:02:13 -0700

    Product tag model with references to the product and tag ids

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,14 +6,35 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+        unique: false,
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+        unique: false,
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
