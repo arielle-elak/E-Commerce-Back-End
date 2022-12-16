0763beaee5d883b20f2e9304394e7b4c0e05d54e
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-15 20:47:26 -0700

    Fixed some typos in the models

--- models/index.js
+++ models/index.js
@@ -11,7 +11,7 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "product_id",
+  foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
@@ -21,7 +21,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false
   },
-  as: 'tagged_products'
+  as: 'product_tags'
 })
 
 
