4902890cda6055bc385d70eb53106b26d0f4007f
Arielle Schlickman-Elak <arielle.elak@gmail.com>
2022-12-15 20:09:15 -0700

    Initial sequelize relationships

--- models/index.js
+++ models/index.js
@@ -5,10 +5,17 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "product_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+
 
 // Tags belongToMany Products (through ProductTag)
 
